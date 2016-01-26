f32c198a1fe42e073211d792c61ebbb674a0ed90
ygreenc <ygreenc@users.noreply.github.com>
2016-01-26 14:32:30 -0500

    Create basic logic for moving file after renaming

--- movieren/main.py
+++ movieren/main.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
 import os.path
+import json
 import click
 
 from config import Config
 from movie import find_likely_movie
+
 
 def build_output_filename(config, movie):
     """Select the output filename.
@@ -16,17 +18,43 @@ def movieren(config, in_file):
     """Main movieren function. Process movie files.
     """
     directory, filename = os.path.split(in_file)
+    _, extension = os.path.splitext(filename)
+
     movie = find_likely_movie(filename)
-    output_filename = build_output_filename(config, movie)
-    print(output_filename)
+
+    # Build destination path
+    if config.get('move_file_enable', False):
+        directory = os.path.expanduser(config['move_file_destination'])
+
+    destination = os.path.join(directory, config['rename_format'] + extension).format(**movie)
+
+    if os.path.exists(destination) and config['overwrite_destination'] is False:
+        raise RuntimeError('File %s already exists' % directory)
+
+    print(destination)
 
 
 @click.command()
 @click.option('--format', help='Ouput format')
+@click.option('--config', help='Use configuration file')
 @click.argument('in_file', type=click.Path(exists=True))
-def main(format, in_file):
+def main(format, config, in_file):
     try:
-        movieren(Config, click.format_filename(in_file))
+        # Configuration parsing
+        # Resolution order is:
+        # 1- Specified at runtime (cli parameter)
+        # 2- ~/.movieren.json
+        # 3- Defaults specified in program
+        if config is None:
+            config = "~/.movieren.json"
+
+        parsed_configuration = Config
+
+        with open(os.path.expanduser(config)) as cnf:
+            loaded_configuration = json.load(cnf)
+            parsed_configuration.update(loaded_configuration)
+
+        movieren(parsed_configuration, click.format_filename(in_file))
     except ValueError as e:
         print(e)
 
