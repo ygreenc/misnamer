3271aba9220b900a16d533ce264183fa038c3e97
ygreenc <ygreenc@users.noreply.github.com>
2016-03-25 13:48:38 -0400

    Move pytest to tests_requires in setup.py

--- setup.py
+++ setup.py
@@ -11,9 +11,9 @@ setup(
     license='MIT',
     install_requires=[
         'requests==2.9.1',
-        'pytest==2.8.5',
         'click==6.2'
     ],
+    tests_require=['pytest==2.8.5'],
     test_suite='tests',
     entry_points={
         'console_scripts': [
