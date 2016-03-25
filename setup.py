319309f8d46805cefe7ac7eda4a0f9e8005429b1
ygreenc <ygreenc@users.noreply.github.com>
2016-03-25 13:35:47 -0400

    Add license to setup.py

--- setup.py
+++ setup.py
@@ -8,10 +8,11 @@ setup(
     url='https://github.com/ygreenc/misnamer',
     packages=['misnamer'],
     description='Rename movie file',
+    license='MIT',
     install_requires=[
-        "requests==2.9.1",
-        "pytest==2.8.5",
-        "click==6.2"
+        'requests==2.9.1',
+        'pytest==2.8.5',
+        'click==6.2'
     ],
     test_suite='tests',
     entry_points={
